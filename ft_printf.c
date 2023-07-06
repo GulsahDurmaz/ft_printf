9b69007d9d5a612a0bb84701f764aa4ad4c3e6b3
Gülsah <73911627+GulsahDurmaz@users.noreply.github.com>
2023-07-06 16:44:18 +0200

    added utils functs

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,77 @@
 /*   By: gdurmaz <gdurmaz@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:04:04 by gdurmaz           #+#    #+#             */
-/*   Updated: 2023/06/30 17:25:59 by gdurmaz          ###   ########.fr       */
+/*   Updated: 2023/07/06 16:42:40 by gdurmaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+size_t	print_count_char(char s)
+{
+	write(1, &s, 1);
+	return (1);
+}
+
+size_t	print_count_args(char *s, va_list args)
+{
+	size_t	counter;
+
+	counter = 0;
+	if (s == 'c')
+		counter = put_count_char(va_arg(args, int));
+	else if (s == 's')
+		counter = put_count_str(va_arg(args, char *));
+	else if (s == 'p')
+		counter = put_count_ptr(va_arg(args, void *), HEX_BASE_LOW, 1);
+	else if (s == 'd' || s == 'i')
+		counter = put_count_nbr_base(va_arg(args, int), DEC_BASE);
+	else if (s == 'u')
+		counter = put_count_unbr_base(va_arg(args, unsigned int), DEC_BASE);
+	else if (s == 'x')
+		counter = put_count_unbr_base(va_arg(args, unsigned int),
+				HEX_BASE_LOW);
+	else if (s == 'X')
+		counter = put_count_unbr_base(va_arg(args, unsigned int),
+				HEX_BASE_UP);
+	else if (s == '%')
+		counter = put_count_char('%');
+	return (counter);
+}
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	args;
 	size_t	counter; //to count the printed characters.
 
-	va_star (args, s);
+	va_start (args, s);
 	counter = 0;
-	while (*s == '%')
+	while (*s)
 	{
+		if (*s == '%')
+		{
+			counter += print_count_args(*++s, args);// to write the args.
+			s++;
+		}
+		else
+			counter += print_count_char(*s++);// print and count the characters.
 
 	}
 	va_end (args);
 	return (counter);
 }
 
+int main()
+{
+	int count;
+	int count2;
 
+	count = ft_printf("Hello World");
+	printf("%d \n", count);
+	count2 = ft_printf("Hello World");
+	printf("%d \n", count2);
+
+}
 /*
     *The printf function will write each character of the intial string,
     one by one, until it finds a %.
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,25 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdurmaz <gdurmaz@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/30 15:52:06 by gdurmaz           #+#    #+#             */
-/*   Updated: 2023/07/06 16:42:35 by gdurmaz          ###   ########.fr       */
+/*   Created: 2023/07/06 14:25:56 by gdurmaz           #+#    #+#             */
+/*   Updated: 2023/07/06 16:43:17 by gdurmaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
+#include "ft_printf.h"
 
-# define NULL_DISPLAY "(null)"
-# define DEC_BASE "0123456789"
-# define HEX_BASE_LOW "0123456789abcdef"
-# define HEX_BASE_UP "0123456789ABCDEF"
-# define POINTER_PREFIX "0x"
-# define NIL_DISPLAY "(nil)"
+size_t	put_count_char(int s)
+{
+	write(1, &s, 1);
+	return (1);
+}
 
+size_t	put_count_str(char *s)
+{
+	size_t	counter;
 
+	if (s == NULL)
+	{
+		write(1, NULL_DISPLAY, ft_strlen(NULL_DISPLAY));
+		return (ft_strlen(NULL_DISPLAY));
+	}
+	counter = 0;
+	while (*s)
+	{
+		write(1, s++, 1);
+		counter++;
+	}
+	return (counter);
+}
